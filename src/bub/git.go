0dd2f0f2c685d626f37c0630a630965b5eb9aa6c
Jean-Martin Archer <jm@jmartin.ca>
2016-10-21 17:48:10 -0700

    formatting

--- src/bub/git.go
+++ src/bub/git.go
@@ -1,22 +1,22 @@
 package main
 
 import (
-	"os/exec"
 	"log"
-	"strings"
+	"os/exec"
 	"path"
+	"strings"
 )
 
 //TODO: Clone/Update All Active Projects
 
-func GetCurrentRepositoryName() (string) {
+func GetCurrentRepositoryName() string {
 	cmd := exec.Command("git", "config", "--get", "remote.origin.url")
 	result, err := cmd.Output()
+
 	if err != nil {
 		log.Fatalf("Error: %v", err)
 	}
+
 	repositoryUri := string(result)
 	return strings.TrimSuffix(path.Base(repositoryUri), path.Ext(repositoryUri))
-
 }
-
