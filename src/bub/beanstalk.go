80a71d795a1443827195ef3f5b9d4d124b0b24ee
Jean-Martin Archer <jm@jmartin.ca>
2016-11-10 21:50:25 -0800

    eb, sort events and environments

--- src/bub/beanstalk.go
+++ src/bub/beanstalk.go
@@ -8,7 +8,37 @@ import (
 	"os"
 	"strings"
 	"text/tabwriter"
+	"sort"
 )
+
+type Environments []*elasticbeanstalk.EnvironmentDescription
+
+func (e Environments) Len() int {
+	return len(e)
+}
+
+func (e Environments) Less(i, j int) bool {
+	return *e[i].ApplicationName < *e[j].ApplicationName
+}
+
+func (e Environments) Swap(i, j int) {
+	e[i], e[j] = e[j], e[i]
+}
+
+type Events []*elasticbeanstalk.EventDescription
+
+func (e Events) Len() int {
+	return len(e)
+}
+
+func (e Events) Less(i, j int) bool {
+	date := *e[i].EventDate
+	return date.After(*e[j].EventDate)
+}
+
+func (e Events) Swap(i, j int) {
+	e[i], e[j] = e[j], e[i]
+}
 
 func getBeanstalkSvc() *elasticbeanstalk.ElasticBeanstalk {
 	sess, err := session.NewSession(&config)
@@ -28,7 +58,10 @@ func ListEnvironments() {
 	}
 
 	fmt.Fprintln(table, "ApplicationName\tEnvironmentName\tStatus\tHealth\tHealthStatus\tVersionLabel\tCNAME")
-	for _, e := range resp.Environments {
+	var environments Environments
+	environments = resp.Environments
+	sort.Sort(environments)
+	for _, e := range environments {
 		row := []string{*e.ApplicationName, *e.EnvironmentName, *e.Status, *e.Health, *e.HealthStatus, *e.VersionLabel, *e.CNAME}
 		fmt.Fprintln(table, strings.Join(row, "\t"))
 	}
@@ -45,12 +78,15 @@ func ListEvents() {
 
 	table := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0)
 	fmt.Fprintln(table, "EventDate\tSev.\tEnvironmentName\tMessage")
-	for _, e := range resp.Events {
+	var events Events
+	events = resp.Events
+	sort.Sort(events)
+	for _, e := range events {
 		time := *e.EventDate
 		var message = *e.Message
 		const limit = 200
 		if len(message) < limit {
-			message = message[0 : len(message)-1]
+			message = message[0 : len(message) - 1]
 		} else {
 			message = message[0:limit] + "..."
 		}
