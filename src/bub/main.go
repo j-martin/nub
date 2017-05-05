482cc3a84ca116bea5092b83d77f8213def62d83
Jean-Martin Archer <jm@jmartin.ca>
2017-05-04 20:38:51 -0700

    fake version bump

--- src/bub/main.go
+++ src/bub/main.go
@@ -32,7 +32,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "bub"
 	app.Usage = "A tool for all your Bench related needs."
-	app.Version = "0.13.15"
+	app.Version = "0.13.16"
 	app.EnableBashCompletion = true
 	app.Commands = []cli.Command{
 		{
