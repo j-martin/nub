6ccb6084140c04ed0649c95a738eb6bdad781233
Jean-Martin Archer <jean-martin@bench.co>
2017-09-29 17:29:32 -0700

    bump version

--- src/bub/main.go
+++ src/bub/main.go
@@ -33,7 +33,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "bub"
 	app.Usage = "A tool for all your Bench related needs."
-	app.Version = "0.18.5"
+	app.Version = "0.18.6"
 	app.EnableBashCompletion = true
 	app.Commands = []cli.Command{
 		{
